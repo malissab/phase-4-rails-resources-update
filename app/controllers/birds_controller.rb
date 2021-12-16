42391e9faff2c5704db59c39150c4515995549f4
malissab <malissambishop7@gmail.com>
2021-12-15 20:06:31 -0800

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,16 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   # POST /birds
   def create
     bird = Bird.create(bird_params)
@@ -22,10 +32,12 @@ class BirdsController < ApplicationController
     end
   end
 
+  
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
